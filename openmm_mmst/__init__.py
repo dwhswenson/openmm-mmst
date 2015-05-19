5fcfa42f1aed8f15ca58ef58952d12ac59f5e6d7
David W.H. Swenson <dwhs@hyperblazer.net>
2015-05-19 02:54:39 +0200

    Working __init__ and dev install

---
+++ openmm_mmst/__init__.py
@@ -0,0 +1,1 @@
+from mmst_integrator import NonadiabaticIntegrator
--- openmm_mmst/mmst_integrator.py
+++ openmm_mmst/mmst_integrator.py
@@ -1,6 +1,6 @@
-from simtk.openmm.opemm import CustomIntegrator
+from simtk.openmm.openmm import CustomIntegrator
 
-class MMSTIntegrator(CustomIntegrator):
+class NonadiabaticIntegrator(CustomIntegrator):
     """Integrator for an MMST trajectory.
 
     Attributes
@@ -9,18 +9,22 @@ class MMSTIntegrator(CustomIntegrator):
     Hmatrix : list of list of System
     """
     def __init__(self, stepSize, Hmatrix):
+        self.Hmatrix = Hmatrix
         pass
 
-class VerletMMSTIntegrator(MMSTIntegrator):
+class VerletMMSTIntegrator(NonadiabaticIntegrator):
     # Exists mainly for testing
     def step(self, nsteps):
         pass
 
-class RungeKuttaMMSTIntegrator(MMSTIntegrator):
+class RungeKuttaMMSTIntegrator(NonadiabaticIntegrator):
     def step(self, nsteps):
         pass
 
 # TODO
-class AdaptiveGear6MMSTIntegrator(MMSTIntegrator):
+class AdaptiveGear6MMSTIntegrator(NonadiabaticIntegrator):
     # see DWHS's dissertation for the discussion of this
     pass
+
+# TODO: actually, this isn't MMST specific: same setup should be usable for
+# surface hopping. But I'll save that for (much) later.
